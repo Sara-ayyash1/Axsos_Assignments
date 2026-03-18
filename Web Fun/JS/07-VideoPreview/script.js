3e43c8a464e2a7f18af8ca7f03e2581d24b0ab77
Sara-ayyash1 <132514820+Sara-ayyash1@users.noreply.github.com>
2026-03-18 13:24:49 +0200

    Cleanup and sync assignments

---
+++ Web Fun/JS/07-VideoPreview/script.js
@@ -0,0 +1,17 @@
+const videos = document.querySelectorAll(".myVideo");
+
+videos.forEach(
+
+  function (video) {
+
+   video.onmouseover = function () {
+    video.style.cursor = "pointer";
+    video.muted = true; 
+    video.play();
+   };
+
+   video.onmouseout = function () {
+    video.pause();
+   };
+  }
+);
