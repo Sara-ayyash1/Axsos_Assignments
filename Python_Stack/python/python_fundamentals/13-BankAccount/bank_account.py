0c1a1696b749ea2e8b83b88f5670bf463669679a
Sara-ayyash1 <132514820+Sara-ayyash1@users.noreply.github.com>
2026-04-14 17:32:57 +0300

    Add Edit Account Assignment (python)

--- Python_Stack/python/python_fundamentals/13-BankAccount/bank_account.py
+++ Python_Stack/python/python_fundamentals/13-BankAccount/bank_account.py
@@ -25,7 +25,7 @@ class BankAccount:
 
 # Create 2 accounts
 account1 =  BankAccount(0.02 , 3000)
-account2 = BankAccount(10)
+account2 = BankAccount(balance=10)
 
 # To the first account, make 3 deposits and 1 withdrawal, then yield interest and display the account's info all in one line of code (i.e. chaining)
 account1.deposit(100).deposit(100).deposit(700).withdraw(100).yield_interest().display_account_info()
