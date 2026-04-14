d275f613a1c19e2e2c42b91032439d720461d151
Sara-ayyash1 <132514820+Sara-ayyash1@users.noreply.github.com>
2026-04-14 15:58:28 +0300

    Add Chaining Methods Assignment (python)

---
+++ Python_Stack/python/python_fundamentals/12-ChainingMethods/user.py
@@ -0,0 +1,43 @@
+class User:
+    def __init__(self , name , balance):
+        self.name = name
+        self.balance = balance
+
+    def make_deposit(self , amount):
+        self.balance += amount
+        return self
+
+    def make_withdrawal(self , amount):
+        self.balance -= amount
+        return self
+
+    def display_user_balance(self):
+        print(f"User: {self.name}, Balance: ${self.balance}")
+        return self
+
+    def transfer_money(self,other_user,amount):
+        self.balance -=amount
+        other_user.balance +=amount
+        return self
+    
+# Create 3 instances of the User class
+user1 = User ("Sara Ayyash" , 1000)
+user2 = User ("Islam" , 30000)
+user3 = User ("Haya" , 800)
+
+# Have the first user make 3 deposits and 1 withdrawal and then display their balance
+user1.make_deposit(100).make_deposit(200).make_deposit(50).make_withdrawal(50)
+user1.display_user_balance()
+
+# Have the second user make 2 deposits and 2 withdrawal and then display their balance
+user2.make_deposit(100).make_deposit(100).make_withdrawal(50).make_withdrawal(100)
+user2.display_user_balance()
+
+# Have the third user make 1 deposits and 3 withdrawal and then display their balance
+user3.make_deposit(500).make_withdrawal(100).make_withdrawal(100).make_withdrawal(50)
+user3.display_user_balance()
+
+#  Add a transfer_money method => have the first user transfer money to the third user and then print both users' balances
+user1.transfer_money(user3, 200)
+user1.display_user_balance()
+user3.display_user_balance()
