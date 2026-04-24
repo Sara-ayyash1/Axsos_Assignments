fee002abbaa683ffad7c93f7ae36a151050cfc28
Sara-ayyash1 <132514820+Sara-ayyash1@users.noreply.github.com>
2026-04-24 20:58:13 +0300

    Add playgroung assignment(python - flask)

--- Python_Stack/flask/flask_fundamentals/03-Playground/playground.py
+++ Python_Stack/flask/flask_fundamentals/03-Playground/playground.py
@@ -2,9 +2,12 @@ from flask import Flask , render_template
 
 app = Flask(__name__)
 
-app.route("/play")
-def play():
-    return render_template("index.html")
+@app.route("/")
+@app.route("/play")
+@app.route("/play/<nums_of_boxes>")
+@app.route("/play/<nums_of_boxes>/<color>")
+def play(nums_of_boxes = 3 , color = "#9FC5F8"):
+    return render_template("index.html",no_of_boxes = int(nums_of_boxes) , clr= color)
 
 
 if __name__ == "__main__":
