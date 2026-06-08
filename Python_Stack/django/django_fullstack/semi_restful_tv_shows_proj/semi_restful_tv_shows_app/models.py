b2f709bd2c4739fd55d80237b761f46675dc7b0d
Sara-ayyash1 <132514820+Sara-ayyash1@users.noreply.github.com>
2026-06-08 18:00:07 +0300

    Add vaildation to create & update method

--- Python_Stack/django/django_fullstack/semi_restful_tv_shows_proj/semi_restful_tv_shows_app/models.py
+++ Python_Stack/django/django_fullstack/semi_restful_tv_shows_proj/semi_restful_tv_shows_app/models.py
@@ -1,6 +1,36 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class ShowManager(models.Manager):
+    def basic_validator(self, postData ,show_id=None):
+        errors = {}
+        if len(postData.get('title' , '').strip()) < 2 :
+            errors['title'] = 'Title should be at least 2 characters'
+            
+        if len(postData.get('network' , '').strip()) < 3 :
+            errors['network'] = 'Network should be at least 3 characters'
+
+        if len(postData.get('description' , '').strip()) > 0 and len(postData.get('description' , '')) < 10 :
+            errors['description'] = 'Description should be at least 10 characters'
+            
+        if postData.get('release_date' , ''):
+            release = datetime.strptime(postData['release_date'], "%Y-%m-%d")
+            if release > datetime.now():
+                errors['release_date'] = 'Release date should be in the past'
+        
+        # Step 1: search for shows with the same title
+        query = Show.objects.filter(title__iexact=postData['title'].strip())
+
+        # Step 2: if update → exclude the current show
+        if show_id:
+            query = query.exclude(id=show_id)
+
+        # Step 3: if result exists → title is not unique
+        if query.exists():
+            errors['title'] = 'Title already exists'
+        return errors
+    
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=45)
@@ -8,6 +38,14 @@ class Show(models.Model):
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager() 
 
     def __str__(self):
         return f"{self.title} ({self.network})"
+    
+
+# if Show.objects.filter(title=postData['title']).exists():
+#     errors['title'] = 'Title already exists'
+# case insensitive
+# if Show.objects.filter(title__iexact=postData['title']).exists():
+#   errors['title'] = 'Title already exists'
--- Python_Stack/django/django_fullstack/semi_restful_tv_shows_proj/semi_restful_tv_shows_app/views.py
+++ Python_Stack/django/django_fullstack/semi_restful_tv_shows_proj/semi_restful_tv_shows_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render , redirect
 from .models import *
+from django.contrib import messages
+
 # Create your views here.
+
 def index(request):
     context ={
         'all_shows' : Show.objects.all()
@@ -14,13 +17,22 @@ def new_show(request):
 
 def create_show(request):
     if request.method == 'POST':
-        new_show = Show.objects.create(
-            title = request.POST['title'], 
-            network = request.POST['network'],
-            release_date = request.POST['release_date'], 
-            description = request.POST['description']
-        )
-        return redirect(f'/shows/{new_show.id}')
+        errors = Show.objects.basic_validator(request.POST)
+
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect(f'/shows/new/')
+        
+        else:
+            new_show = Show.objects.create(
+                title = request.POST['title'].strip(),
+                network = request.POST['network'].strip(),
+                release_date = request.POST['release_date'],
+                description = request.POST['description'].strip()
+            )
+            messages.success(request, "Show successfully created.")
+            return redirect(f'/shows/{new_show.id}')
     
     return redirect('/shows/new/') 
 
@@ -44,17 +56,36 @@ def edit_show(request , show_id):
 
 
 def update_show(request, show_id):
-    show_to_update = Show.objects.get(id = show_id)
-    if request.method == 'POST':    
-        show_to_update.title = request.POST['title']
-        show_to_update.network = request.POST['network']
-        show_to_update.release_date = request.POST['release_date']
-        show_to_update.description = request.POST['description']
-        show_to_update.save()
+    if request.method == 'POST':
+        # Pass the POST data to the method we wrote and save the response in a variable called errors.
+        errors = Show.objects.basic_validator(request.POST , show_id)
+
+        # Check if the errors dictionary has anything in it.
+        if len(errors) > 0:
+            # If the errors dictionary contains anything, loop through each key-value pair and make a flash message.
+            for key, value in errors.items():
+                messages.error(request, value)
+            # Redirect the user back to the form to fix the errors.
+            return redirect(f'/shows/{show_id}/edit/')
         
-        return redirect(f'/shows/{show_id}')
+        else:
+            # If the errors dictionary is empty, that means there were no errors.
+            # Retrieve the Show to be updated, make the changes, and save.
+            show_to_update = Show.objects.get(id = show_id)
+   
+            show_to_update.title = request.POST['title']
+            show_to_update.network = request.POST['network']
+            show_to_update.release_date = request.POST['release_date']
+            show_to_update.description = request.POST['description']
+            show_to_update.save()
+            
+            messages.success(request, "Show successfully updated.")
+            # Redirect to a success route.
+            return redirect(f'/shows/{show_id}')
     
     return redirect(f'/shows/{show_id}/edit/')
+
+
 
 
 # from django.utils import timezone
